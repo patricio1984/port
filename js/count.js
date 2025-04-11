55c6104cf196d0dc124d02442422cf1d8a677e24
Patricio M. <patricio1984@gmail.com>
2025-04-11 18:35:10 -0300

    fixing visitor script - doing the callback function

--- js/count.js
+++ js/count.js
@@ -1,8 +1,16 @@
 
 function mostrarVisitas(data) {
-	document.getElementById('count').textContent = data.visitas;
+	const countEl = document.getElementById('count');
+	if (data && data.visitas !== undefined) {
+		countEl.textContent = data.visitas;
+	} else {
+		countEl.textContent = 'Error 😢';
+	}
 }
 
-  const script = document.createElement('script');
-  script.src = 'https://script.google.com/macros/s/AKfycbwZrlB6VelnVc_lWh9Kmn56UiiT_qHL7Dji2xGOu9GnvMqsycKpbNYeuVcg82m0b9HT6A/exec?callback=mostrarVisitas';
-  document.body.appendChild(script);
+(function () {
+	const script = document.createElement('script');
+	script.src = 'https://script.google.com/macros/s/AKfycbwZrlB6VelnVc_lWh9Kmn56UiiT_qHL7Dji2xGOu9GnvMqsycKpbNYeuVcg82m0b9HT6A/exec?callback=mostrarVisitas';
+	document.body.appendChild(script);
+})();
+  
