b9ed87267e416c611c06a2a58854379f4010ef8c
Patricio M. <patricio1984@gmail.com>
2025-04-11 18:27:26 -0300

    creating new visitor script

--- js/count.js
+++ js/count.js
@@ -3,9 +3,13 @@ const countEl = document.getElementById('count');
 updateVisitCount();
 
 function updateVisitCount() {
-	fetch('https://api.countapi.xyz/update/patriciom/elgnomo/?amount=1')
-	.then(res => res.json())
-	.then(res => {
-		countEl.innerHTML = res.value;
-	})
+  fetch('https://script.google.com/macros/s/AKfycbzTbO6D1AdQyUpNtkJyJce6L9En91cxStzb2QMm9Yt6fD2YVYhO6qP26bJ9uGty1rlQ/exec')
+    .then(res => res.json())
+    .then(res => {
+      countEl.innerHTML = res.visitas;
+    })
+    .catch(err => {
+      console.error('Error al contar visitas:', err);
+      countEl.innerHTML = 'Error 😢';
+    });
 }
