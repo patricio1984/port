d76a533b429a874710d64f546a637ddabe98a805
Patricio M. <patricio1984@gmail.com>
2025-04-26 01:14:20 -0300

    uploading new images

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,7 @@
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Main.scss';
-import profileImg from '../assets/images/profile.png';
+import profileImg from '../assets/images/profile.webp';
 
 function Main() {
 
