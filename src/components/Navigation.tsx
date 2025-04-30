927c871324c007f13ecf747e37a1fa1891f60a9c
Patricio M. <patricio1984@gmail.com>
2025-04-30 17:45:44 -0300

    fixing the footer and the project title

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -17,7 +17,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 
 const drawerWidth = 240;
-const navItems = [['Experiencia Técnica', 'expertise'], ['Experiencia Laboral', 'history'], ['Projectos', 'projects'], ['Contacto', 'contact']];
+const navItems = [['Experiencia Técnica', 'expertise'], ['Experiencia Laboral', 'history'], ['Proyectos', 'projects'], ['Contacto', 'contact']];
 
 function Navigation({parentToChild, modeChange}: any) {
 
